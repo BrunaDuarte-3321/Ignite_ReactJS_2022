0f61798581945b5f3e92880aee3f3c3a43741e54
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2022-11-16 09:50:11 -0300

    coffee-delivery/ Finalizando configurações inicial para desenvolivmento do projeto.

---
+++ coffee-delivery/src/Routes.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+import { CompleteOrder } from './pages/CompleteOrder'
+import { HomePage } from './pages/Home'
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/complete" element={<CompleteOrder />} />
+    </Routes>
+  )
+}
