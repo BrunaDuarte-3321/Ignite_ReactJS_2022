554bc9c55c333f3278d24e31a4984b2253d63542
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2022-12-28 15:13:59 -0300

    Adicionando component Transictions, Header, Summary da aplicação.

--- 03-DT-MONEY/src/App.tsx
+++ 03-DT-MONEY/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
 import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/theme/global'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <p>Hello World!</p>
-      </div>
+      <Transactions />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
