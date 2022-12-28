4da1b3290ba490c52658ce3e50f77c5e003f7d95
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2022-12-28 16:23:32 -0300

    Adicionando component de buscar na aplicação.

--- 03-DT-MONEY/src/pages/Transactions/index.tsx
+++ 03-DT-MONEY/src/pages/Transactions/index.tsx
@@ -1,11 +1,42 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHightLight,
+  TransactionsContainer,
+  TransctionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransctionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightLight variant="income">R$ 12.000,00</PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHightLight variant="outcome">- R$ 59,00</PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransctionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
