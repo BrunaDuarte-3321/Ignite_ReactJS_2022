001702eaeffaba20abf27abbe21675797f6bb82c
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-04-12 14:54:06 -0300

    modulo 5

--- 04-Fundamentos_NextJS/ignite-shop/src/pages/index.tsx
+++ 04-Fundamentos_NextJS/ignite-shop/src/pages/index.tsx
@@ -1,29 +1,78 @@
-import { HomeContainer, Product } from "../styles/pages/home";
 import Image from 'next/image'
+import Head from 'next/head';
+import {useKeenSlider} from 'keen-slider/react'
+import { stripe } from "../lib/stripe";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import 'keen-slider/keen-slider.min.css'
+import { HomeContainer, Product } from "../styles/pages/home";
+import Link from 'next/link';
 
 
-import camiseta1 from '../assets/tshirt1.png'
-import camiseta2 from '../assets/tshirt2.png'
-import camiseta3 from '../assets/tshirt3.png'
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+  } []
+}
+export default function  Home({products}:HomeProps) {
 
-export default function  Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+     perView:3,
+     spacing: 48,
+    }
+  })
   return (
-   <HomeContainer>
-      <Product>
-       <Image src={camiseta1} width={520} height={480} alt=""/>
+   <>
+   <Head>
+    <title>Home - Ignite Shop</title>
+   </Head>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+    {products.map((product) => (
+      <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+        <Product className="keen-slider__slide" >
+       <Image src={product.imageUrl} width={520} height={480} alt=""/>
        <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,90</span>
+        <strong>{product.name}</strong>
+        <span>{product.price}</span>
        </footer>
       </Product>
-      <Product>
-       <Image src={camiseta1} width={520} height={480} alt=""/>
-       <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,90</span>
-       </footer>
-      </Product>
+      </Link>
+      
+    ))}
    </HomeContainer>
+   </>
   )
 }
+
+// eslint-disable-next-line @next/next/no-typos
+export const getStaticProps:GetStaticProps = async ()  => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100) ,
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+  } 
+}
+
 
---
+++ 05-design-system/README.md
@@ -0,0 +1,11 @@
+# Components:
+
+- [ ]  Text
+- [ ]  Header
+- [ ]  Box
+- [ ]  Button
+- [ ]  TextInput
+- [ ]  TextArea
+- [ ]  Checkbox
+- [ ]  Avatar
+- [ ]  MultStep
