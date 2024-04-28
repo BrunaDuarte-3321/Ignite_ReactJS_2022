9dc081bdb3cad47570e66f7ba8b8df9fdb0ade42
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2024-04-28 14:05:54 -0300

    feat: adicionando carousel com keen-slider

--- 04-Fundamentos_NextJS/ignite-shop/src/pages/index.tsx
+++ 04-Fundamentos_NextJS/ignite-shop/src/pages/index.tsx
@@ -1,14 +1,25 @@
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 import Image from 'next/image'
 import shirt1 from '../assets/T-Shirt/shirt1.png'
 import shirt2 from '../assets/T-Shirt/shirt2.png'
 import shirt3 from '../assets/T-Shirt/shirt3.png'
 
 export default function Home() {
+  const [slierRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
   return (
-    <main className="flex gap-12 w-full max-w-screen-customScreen ml-auto min-h-[656px]">
+    <main
+      className="keen-slider flex w-full max-w-screen-customScreen ml-auto min-h-[656px] "
+      ref={slierRef}
+    >
       <a
         href=""
-        className="relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
+        className="relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group keen-slider__slide"
       >
         <Image
           src={shirt1}
@@ -24,7 +35,7 @@ export default function Home() {
       </a>
       <a
         href=""
-        className="relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
+        className="keen-slider__slide relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
       >
         <Image src={shirt2} width={520} height={480} alt="" />
         <footer className="absolute bottom-1 left-1 right-1 rounded-[6px] flex items-center p-8 justify-between bg-black/[0.6] translate-y-[110%] opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-[0%] group-hover:opacity-100">
@@ -34,7 +45,27 @@ export default function Home() {
       </a>
       <a
         href=""
-        className="relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
+        className="keen-slider__slide relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
+      >
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer className="absolute bottom-1 left-1 right-1 rounded-[6px] flex items-center p-8 justify-between bg-black/[0.6] translate-y-[110%] opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-[0%] group-hover:opacity-100">
+          <strong className="text-xl">Camiseta 3</strong>
+          <span className="text-2xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+      <a
+        href=""
+        className="keen-slider__slide relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
+      >
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer className="absolute bottom-1 left-1 right-1 rounded-[6px] flex items-center p-8 justify-between bg-black/[0.6] translate-y-[110%] opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-[0%] group-hover:opacity-100">
+          <strong className="text-xl">Camiseta 3</strong>
+          <span className="text-2xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+      <a
+        href=""
+        className="keen-slider__slide relative bg-gradient-to-r from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer flex items-center justify-center opacity-[1] group"
       >
         <Image src={shirt3} width={520} height={480} alt="" />
         <footer className="absolute bottom-1 left-1 right-1 rounded-[6px] flex items-center p-8 justify-between bg-black/[0.6] translate-y-[110%] opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-[0%] group-hover:opacity-100">
