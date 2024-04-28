1a87391e21bb5dd63fabb5314c2e5b68597d01b3
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2024-04-28 11:01:53 -0300

    feat: adicionando header da aplicação

--- 04-Fundamentos_NextJS/ignite-shop/src/pages/_app.tsx
+++ 04-Fundamentos_NextJS/ignite-shop/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import logoImage from '../assets/logo.svg'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col items-start justify-center min-h-screen">
+      <header className="py-2 px-0 w-full max-w-[1180px]  m-auto">
+        <Image src={logoImage} alt="logo ignite Shop" />
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
